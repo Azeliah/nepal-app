c5b7b8ea931df00b350a6acf89606ac597bf4811
Azeliah <71652004+Azeliah@users.noreply.github.com>
2021-12-02 15:24:50 +0100

    Revert "Recipes logic"

--- app/src/main/java/com/example/sundmadnepal/data/Datasource.kt
+++ app/src/main/java/com/example/sundmadnepal/data/Datasource.kt
@@ -4,12 +4,12 @@ import com.example.sundmadnepal.R
 import com.example.sundmadnepal.model.Recipe
 
 class Datasource {
-    fun loadRecipes(): List<Recipe> {
+    fun loadRecipes(): List<Recipe>{
         return listOf<Recipe>(
-            Recipe(R.string.dahl, R.drawable.dahl, null, null),
-            Recipe(R.string.momo, R.drawable.momo, null, null),
-            Recipe(R.string.fried_chicken, R.drawable.fried_chicken, null, null),
-            Recipe(R.string.newari, R.drawable.newarithali, null, null)
+            Recipe(R.string.dahl, R.drawable.dahl),
+            Recipe(R.string.momo, R.drawable.momo),
+            Recipe(R.string.fried_chicken, R.drawable.fried_chicken),
+            Recipe(R.string.newari, R.drawable.newarithali)
         )
     }
 }
