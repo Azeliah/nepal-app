a41d5df0f2bcda496449375442b5b1b577194d5f
Azeliah <s205460@student.dtu.dk>
2021-12-02 15:28:53 +0100

    Made ingredientList and procedure nullable

--- app/src/main/java/com/example/sundmadnepal/model/Recipe.kt
+++ app/src/main/java/com/example/sundmadnepal/model/Recipe.kt
@@ -7,6 +7,6 @@ import androidx.annotation.StringRes
 data class Recipe(
     @StringRes val recipeNameId: Int,
     @DrawableRes val imageResourceId: Int,
-    val ingredientList: List<IngredientMeasure>,
-    val procedure: List<ProcedureStep>
+    val ingredientList: List<IngredientMeasure>?,
+    val procedure: List<ProcedureStep>?
 )
