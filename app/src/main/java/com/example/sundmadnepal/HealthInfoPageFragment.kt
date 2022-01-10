6dc02601f55902426719da4cd53bed54581034e0
Adam Harald Jørgensen <adamharald440@gmail.com>
2022-01-10 17:21:13 +0100

    Renamed health info page fragment layout in accordance with 0ad4c1d

--- app/src/main/java/com/example/sundmadnepal/HealthInfoPageFragment.kt
+++ app/src/main/java/com/example/sundmadnepal/HealthInfoPageFragment.kt
@@ -30,7 +30,7 @@ class HealthInfoPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_health_info_specific, container, false)
+        return inflater.inflate(R.layout.fragment_health_info_page, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
