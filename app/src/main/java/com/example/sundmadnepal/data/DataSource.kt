61a1a0163176d207b6fc2e6460a41c7718219852
Jens <jens_jens_kloster@hotmail.com>
2022-01-11 11:51:08 +0100

    Set up dummy data for recipes. Changed Step adapter so that if an image is null, it will not still fill up the recyclerview it will instead be hidden.

--- app/src/main/java/com/example/sundmadnepal/data/DataSource.kt
+++ app/src/main/java/com/example/sundmadnepal/data/DataSource.kt
@@ -10,7 +10,7 @@ fun loadRecipes(): List<Recipe> {
 
     // This is just dummy data, until we can implement a proper repository for recipe data
     return listOf(
-        Recipe(
+        /*Recipe(
             id = 1,
             name = R.string.dahl,
             image = R.drawable.dahl
@@ -40,6 +40,37 @@ fun loadRecipes(): List<Recipe> {
                 Ingredient(2, R.drawable.red_lentils, "1½ cup of lentils"),
                 Ingredient(3, R.drawable.potatoes, "3 potatoes in either squares or whole"))
 
+        )*/
+        Recipe(
+            id = 1,
+            name = R.string.nutritious_flower,
+            image = R.drawable.nutritiousflower,
+            stepList = listOf(
+                Step(1, R.drawable.grainsfornutritiousflour, "पोषिलो पिठो अन्न समूहबाट भिन्न किसिमका २ भाग अन्न  (चामल, गहुँ, मकै, कोदो, जौ, फापर आदि मध्येबाट १÷भाग)\n"),
+                Step(2, null, " र १ भाग गेडागुडी (भटमास, चना केराउ मध्ये आदि)\n"),
+                Step(3, null, " लाई छुट्टाछुट्टै केलाउने र राम्ररी पाक्ने गरि भुट्ने र छुट्टाछुट्टै पिधेर तयार गरिएको पिठोलाई पकाउँदा साग, गाजर , दूध, घ्यू, मह, अण्डा, मासु, आयोडिनयुक्त नुन मिसाई तयार गर्न सकिन्छ ।"),
+                Step(4, R.drawable.sortoutthegrains, "१"),
+                Step(5, R.drawable.roastthegrains, "२"),
+                Step(6, R.drawable.grindthegrains, "३"),
+                Step(7, R.drawable.putflourincontainer, "४"))
+        ),
+        Recipe(
+            id = 2,
+            name = R.string.jaulo,
+            image = R.drawable.jaulo_finished,
+            ingredientList = listOf(
+                Ingredient(1, R.drawable.rice_for_jaulo, "चमल       २५ ग्राम"),
+                Ingredient(2, R.drawable.lentil_for_jaulo, "मुंग वा मुसुरो वा रहको वा कालो दाल ८ ग्राम"),
+                Ingredient(3, R.drawable.oiljaulo, "घ्यू वा तेल    ५ ग्राम"),
+                Ingredient(4, R.drawable.spinach_saag_for_jaulo, "हरिायो सागपात  इच्छा अनुसार")
+            ),
+            stepList = listOf(
+                Step(1, null, "१: चामल र दाललाई केलाउने ।"),
+                Step(2, null, "२: चामल र दाललाईमिासएर थोरै पानीले आभा घण्टा सम्म भिजाउने ।\n"),
+                Step(3, null, "३: आधा घण्टा पछि थोरै घ्यू र पानी हालेर छोपेर पकाउने ।"),
+                Step(4, null, "४: पाक्दै गरेको जाउलोमा पिसेको वा मसिनो पारेर टुक्रा गरेको साग पनि मिसाउने र एकछिन छोपेर पकाउने।\n"),
+                Step(5, null, "५: स्वादको लागि थोरै आयोडिन युक्त नून हाल्ने ।")
+            )
         )
     )
 }
